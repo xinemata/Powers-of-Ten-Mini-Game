46721b1399d041fbe51ac149b41fe873b5d97ef5
xinemata <contact@xin-xin.info>
2019-09-17 01:14:59 -0400

    Initial Commit

---
+++ Assets/CameraLookAtBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAtBall : MonoBehaviour
+{
+
+    public GameObject ball;
+    Vector3 lookAtOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAtOffset = new Vector3(0, 1.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //look at ball
+        transform.LookAt(ball.transform.position + lookAtOffset);
+    }
+}
---
+++ Assets/StickyBall.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyBall: MonoBehaviour {
+
+  // get input for the facing angle
+  public float facingAngle = 0;
+
+  // get input for the x and z for the ball
+  float x = 0;
+  float z = 0;
+
+  Vector2 unitv2;
+
+  public GameObject cameraReference;
+  float distanceToCamera = 5;
+
+  // ball size, starting at 1
+  float size = 1;
+
+  // adding different groups
+  public GameObject group1;
+  bool group1Unlocked = false;
+
+  public GameObject group2;
+  bool group2Unlocked = false;
+
+  public GameObject group3;
+  bool group3Unlocked = false;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+    // user controls
+    x = Input.GetAxis("Horizontal") * Time.deltaTime * -100;
+    z = Input.GetAxis("Vertical") * Time.deltaTime * 500;
+
+    // Facing Angle
+    facingAngle += x;
+    unitv2 = new Vector2(Mathf.Cos(facingAngle * Mathf.Deg2Rad), Mathf.Sin(facingAngle * Mathf.Deg2Rad));
+
+  }
+
+  private void FixedUpdate() {
+
+    // Apply force behidn the ball 
+    this.transform.GetComponent < Rigidbody > ().AddForce(new Vector3(unitv2.x, 0, unitv2.y) * z * 3);
+
+    // Set Camera Position Behidn the Ball based on rotation 
+    cameraReference.transform.position = new Vector3(-unitv2.x * distanceToCamera, distanceToCamera, -unitv2.y * distanceToCamera) + this.transform.position;
+
+    unlockPickupGroups();
+
+  }
+
+// User-defined function that contains boolean expression for the three groups
+    
+  void unlockPickupGroups()
+  {
+    if (group1Unlocked == false) {
+
+      if (size >= 1) {
+        group1Unlocked = true;
+        for (int i = 0; i < group1.transform.childCount; i++) {
+          group1.transform.GetChild(i).GetComponent < Collider > ().isTrigger = true;
+        }
+      }
+    } else if (group2Unlocked == false) {
+
+      if (size >= 1.8f) {
+        group2Unlocked = true;
+        for (int i = 0; i < group2.transform.childCount; i++) {
+          group2.transform.GetChild(i).GetComponent < Collider > ().isTrigger = true;
+        }
+        // Change distance between camera and ball
+        distanceToCamera = 6;
+      }
+    } else if (group3Unlocked == false) {
+
+      if (size >= 2.8f) {
+        group3Unlocked = true;
+        for (int i = 0; i < group3.transform.childCount; i++) {
+          group3.transform.GetChild(i).GetComponent < Collider > ().isTrigger = true;
+        // Change distance between camera and ball
+        distanceToCamera = 6.3f;
+        }
+      }
+
+    }
+  }
+    
+  // Pick up Sticky Objects
+  private void OnTriggerEnter(Collider other) {
+    if (other.transform.CompareTag("Sticky")) {
+      if (0 < size) {
+
+        // Grow the Sticky Ball
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+        size += 0.1f;
+
+        // Disable so that the objects will only stick to your sphere
+        other.enabled = false;
+
+        // Becomes Child so it stays with the ball
+        other.transform.SetParent(this.transform);
+
+        // Print to Console, works like println () in Processing or print() in p5
+        Debug.Log(size);
+          
+      }
+    }
+  }
+}

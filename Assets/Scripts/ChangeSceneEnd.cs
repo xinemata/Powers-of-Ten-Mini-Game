8a44cad017177ce6e1c89934d35de2e40ba08c0a
xinemata <contact@xin-xin.info>
2019-09-27 11:56:05 -0400

    Added scene change mechanism
    ChangeScene scripts to transition scene to scene using key pressed and trigger collider

---
+++ Assets/Scripts/ChangeSceneEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneEnd : MonoBehaviour
+{
+    void Update()
+    {
+        // Press the space key to start coroutine
+    }
+
+    private void OnTriggerEnter(Collider other) {
+    if (other.transform.CompareTag("EndGame")) {
+        
+        // load scene 3 upong collision
+        SceneManager.LoadScene("Scene3", LoadSceneMode.Single);
+
+        // Print to Console, works like println () in Processing or print() in p5
+        Debug.Log("Game Over Cube is triggered");
+    }
+  }
+}
---
+++ Assets/Scripts/ChangeSceneRestart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneRestart : MonoBehaviour
+{
+    void Update()
+    {
+        // Press the space key to start coroutine
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("key is pressed");
+            // Use a coroutine to load the Scene in the background
+            StartCoroutine(LoadYourAsyncScene());
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Scene1");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}

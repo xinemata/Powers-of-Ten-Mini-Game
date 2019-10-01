37dba55d3957a266cdab04693be915b8b0224209
xinemata <contact@xin-xin.info>
2019-10-01 12:14:01 -0400

    Added lock rotation script

---
+++ Assets/Scripts/LockRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockRotation : MonoBehaviour
+{
+Quaternion rotation; 
+Vector3 position;
+
+public int height = 3;
+ 
+void Awake(){
+    rotation = transform.rotation;
+
+    position.y = height;
+
+
+}
+void LateUpdate()
+{
+     transform.rotation = rotation; 
+     
+     position.y = height;
+}
+}
+ 
+ 
